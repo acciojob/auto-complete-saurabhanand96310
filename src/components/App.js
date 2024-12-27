b5b808bddc6bce6203017583b8bd18f0d3121aca
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-12-27 19:19:34 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
+ const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
 const App = () => {
+     const [Data, setData]=useState("")
+
+    const handleChande=(e)=>{
+      setData(e.target.value)
+    }
+   const filteredFruits= fruits.filter((item)=>item.toLowerCase().includes(Data.toLowerCase()))
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search item</h1>
+        <input placeholder="search" value={Data} onChange={handleChande}></input>
+        {
+          filteredFruits.map(item=>{
+            return(
+              <ul>
+                <li>{item}</li>
+              </ul>
+            )
+          })
+        }
     </div>
   )
 }
